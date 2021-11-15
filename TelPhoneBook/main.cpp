603c8284d444ba4203062299c6999489ed4b311d
Pdom <ahb0327@naver.com>
2021-11-15 09:27:36 +0900

    Initial Commit

---
+++ TelPhoneBook/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TelPhoneBook
+//
+//  Created by Pdom on 2021/11/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
